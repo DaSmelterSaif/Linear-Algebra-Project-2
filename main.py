853096b2ea3a983be2ede1ee848cd924b3ff0243
Saif Aldin Sunjaq <saifmarwan774@gmail.com>
2025-04-29 17:01:50 +0400

    project finished :D

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# NAMES HERE (IT WON'T BE ON GITHUB LOL)
+
 # A 3x3 array with zeros.
 matrix: np.ndarray = np.zeros((3, 3))
 
@@ -18,23 +20,46 @@ for i in range(1, 3):
 
 matrix[2][2] = float(input("a33: "))
 
-print(matrix)
+print("-------------------")
+print(matrix, end="\n\n")
 
 def computeEigenVector(A)-> np.ndarray:
     eigenvalues: list = [A[0][0], A[1][1], A[2][2]]
-    tempMatrix: np.ndarray = A.copy()
+    tempMatrix: np.ndarray = np.zeros((3, 3))
 
-    # for value in eigenvalues:
-    #     pass
+    # When solving on a separate sheet, the eigenvectors
+    # follow the following pattern:
+    # s*[1 0 0]^T for a11
+    # s*[(some number) 1 0]^T for a22
+    # s*[(some number) (some number) 1]^T for a33
+    # Calculating the already known values is redundant,
+    # so I have kept it as is in the code.
 
-    # Eigenvalues[0]
-    value = eigenvalues[0]
-    tempMatrix[0][0] = 0
-    tempMatrix[1][1] = tempMatrix[1][1] - value
-    tempMatrix[2][2] = tempMatrix[2][2] - value
-    # TODO - Add the rest of the code to compute the eigenvector.
-    # TODO - Automate using the for loop.
+    # Eigenvector for a11
+    tempMatrix[0][0] = 1
+    tempMatrix[1][0] = 0
+    tempMatrix[2][0] = 0
+
+    # Eigenvector for a22
+    # -c12 / (c11 - c22)
+    tempMatrix[0][1] = -A[0][1] / (A[0][0] - A[1][1])
+    tempMatrix[1][1] = 1
+    tempMatrix[2][1] = 0
+
+    # Eigenvector for a33
+    # (c23c12) / (c22 - c33)(c11 - c33) - c13/(c11 - c33)
+    tempMatrix[0][2] = (A[1][2] * A[0][1]) / ((A[1][1] - A[2][2])*(A[0][0] - A[2][2]))
+    tempMatrix [0][2] = tempMatrix[0][2] - A[0][2] / (A[0][0] - A[2][2])
+    # -c23 / (c22 - c33)
+    tempMatrix[1][2] = -A[1][2] / (A[1][1] - A[2][2])
+    tempMatrix[2][2] = 1
 
     return tempMatrix
 
-print(computeEigenVector(matrix))
+eigenVectors = computeEigenVector(matrix)
+
+# ############ Output ############
+for columns in range(3):
+    print("Eigenvector for %d:" % (matrix[columns][columns]))
+    print("s*(%.3f, %.3f, %.3f)^T\n" % (eigenVectors[0][columns],
+            eigenVectors[1][columns], eigenVectors[2][columns]))
